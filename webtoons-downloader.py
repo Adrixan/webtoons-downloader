a0c91487050b5af4810196f6d3855617b0120fd4
Peter Aufner <peter@code-alongsi.de>
2020-03-04 17:46:48 +0100

    Switched fro image stitching to creation of html to handle animated content

--- webtoons-downloader.py
+++ webtoons-downloader.py
@@ -12,27 +12,52 @@ urllib.request.install_opener(opener)
 def make_req(url):
     return urllib.request.urlopen(url).read().decode('utf-8')
 
-def download_image(url, directory, number):
-    urllib.request.urlretrieve(url, directory + '/' + str(number) + '.jpg')
+def get_power(remainder):
+    power_of_number = 0
+    while remainder >= 10:
+        remainder /= 10
+        power_of_number += 1
+    return power_of_number
 
-def create_image(dir, name):
-    images = [Image.open(dir + '/' + x) for x in os.listdir(dir)]
-    widths, heights = zip(*(i.size for i in images))
 
-    max_width = max(widths)
-    total_height = sum(heights)
+def download_image(url, directory, number, max_zeros):
+    number_string = ""
+    remainder = number
+    power_of_number = get_power(number)
+    for _ in range(max_zeros - power_of_number):
+        number_string += "0"
+    number_string += str(number)
+    extension = '.jpg'
+    if '.gif' in url:
+        extension = '.gif'
+    urllib.request.urlretrieve(url, directory + '/' + number_string + extension)
 
-    new_im = Image.new('RGB', (max_width, total_height))
+def create_html(dir, title):
+    html = '''<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+            <html>
+            <head>
+                <meta content="text/html; charset=ISO-8859-1" http-equiv="content-type">
+                <title>''' + title + '''</title>
+            </head>
+            <body>'''
 
-    y_offset = 0
-    for i in images:
-        new_im.paste(i, (0, y_offset))
-        y_offset += i.size[1]
+    images = os.listdir(dir)
+    images.sort()
+    for image in images:
+        html += '<img src="' + image + '"> <br>'
+    html += '''</body>
+    </html>'''
+    with open(dir + "/index.html", 'w') as output:
+        output.write(html)
 
-    new_im.save(name + '.png')
+def create_dir_if_not_exist(folder):
+    CHECK_FOLDER = os.path.isdir(folder)
+
+    if not CHECK_FOLDER:
+        os.makedirs(folder)
+        print("created folder: ", folder)
 
 def main():
-    tmp_dir = 'tmp'
     site = BeautifulSoup(make_req(sys.argv[1]), 'html.parser')
     imagelist = site.find(id="_imageList")
     images = imagelist.find_all('img')
@@ -40,23 +65,23 @@ def main():
     for i in images:
         imagelinks.append(i.get('data-url'))
 
-    title = ''.join(e for e in site.title.string if e.isalnum())
+    title = site.title.string
+    title_parts = title.split('|')
+    series = title_parts[1][1:]
+    episode = title_parts[0][:-1]
+    destination = series + '/' + episode
 
-    CHECK_FOLDER = os.path.isdir(tmp_dir)
+    create_dir_if_not_exist(series)
+    create_dir_if_not_exist(destination)
 
-    if not CHECK_FOLDER:
-        os.makedirs(tmp_dir)
-        print("created folder: ", tmp_dir)
+    number_of_images = len(imagelinks)
 
     print("Downloading images for: ", title)
     for i in range(len(imagelinks)):
-        download_image(imagelinks[i], tmp_dir, i)
+        download_image(imagelinks[i], destination, i, get_power(number_of_images))
 
-    print("Creating final image")
-    create_image('tmp', title)
-
-    print('Deleting temporary files')
-    rmtree('tmp')
+    print("Creating final html")
+    create_html(destination, title)
 
 if __name__ == "__main__":
     main()
